0f69f0b30e622bf4759c26a60c2ca7d4aec55bb7
bastian <bastianort@gmail.com>
2018-06-01 21:16:38 -0400

    hola

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  root 'pages#index'
+  get 'students/index'
+  root 'students#index'
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
